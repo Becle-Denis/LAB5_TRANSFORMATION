ef9974fd6daa89b1b5b79ff012af3936db9f2717
Denis <c00254311@itcarlow.ie>
2019-11-27 21:16:07 +0000

    Vector 3 -

--- Transformation/Transformation/Vector3.cpp
+++ Transformation/Transformation/Vector3.cpp
@@ -49,9 +49,9 @@ Vector3 operator+(Vector3 const& v1, Vector3 const& v2)
 	return Vector3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
 }
 
-Vector3 operator-(Vector3 const& v1, Vector3 const& c2)
+Vector3 operator-(Vector3 const& v1, Vector3 const& v2)
 {
-	return Vector3();
+	return Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
 }
 
 Vector3 operator-(Vector3 const& v1)
