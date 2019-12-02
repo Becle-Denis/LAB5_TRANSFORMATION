9626c8491f6f6b7cbd95805b963e06f7a5ceac6b
(Student) - Denis Bastien Becle <c00254311@ITCARLOW.IE>
2019-12-02 10:34:19 +0000

    Matrix 3 -

--- Transformation/Transformation/Main.cpp
+++ Transformation/Transformation/Main.cpp
@@ -161,6 +161,16 @@ int main()
 	m3 = m1 + m2;
 	std::cout << "A + B = " << m3.toString() << std::endl << std::endl;
 
+	//  -
+	std::cout << "----- A - B ----" << std::endl;
+	m1 = Matrix3(5, 3, 8, 3, 4, -6, 1, 5, 8);
+	m2 = Matrix3(-4, 2, 6, 12, 3, 0, 2, 4, 2);
+	std::cout << "A = " << m1.toString() << std::endl;
+	std::cout << "B = " << m2.toString() << std::endl;
+	m3 = m1 - m2;
+	std::cout << "A - B = " << m3.toString() << std::endl << std::endl;
+
+
 
 
 	return 0;
